307e103db247922a1122086e73cad08d6faa9dae
Aaryan Singh <134678909+aaryxnblondead@users.noreply.github.com>
2026-04-25 11:13:21 +0530

    feat: add initial project configuration files for IntelliJ IDEA

--- anora_frontend/anora/android/app/build.gradle.kts
+++ anora_frontend/anora/android/app/build.gradle.kts
@@ -19,15 +19,16 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+defaultConfig {
         applicationId = "com.example.anora"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+    }
+
+androidResources {
+        noCompress += "tflite"
     }
 
     buildTypes {
--- anora_frontend/anora/android/app/proguard-rules.pro
+++ anora_frontend/anora/android/app/proguard-rules.pro
@@ -1,3 +1,6 @@
 # Keep rules for TensorFlow Lite GPU delegate classes referenced by tflite_flutter.
 -dontwarn org.tensorflow.lite.gpu.GpuDelegateFactory$Options$GpuBackend
 -dontwarn org.tensorflow.lite.gpu.GpuDelegateFactory$Options
+1
+# Protects the core TensorFlow Lite engine from being deleted during the Release build
+-keep class org.tensorflow.lite.** { *; }
